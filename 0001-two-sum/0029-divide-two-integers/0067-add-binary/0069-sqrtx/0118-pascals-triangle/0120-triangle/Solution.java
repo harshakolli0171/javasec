8fd9f84bebc17ebfdaec9686d8dbedc8cae88bc9
harshakolli0171 <eng25am0171@dsu.edu.in>
2026-09-11 15:03:52 +0530

    Create Solution.java

---
+++ 0001-two-sum/0029-divide-two-integers/0067-add-binary/0069-sqrtx/0118-pascals-triangle/0120-triangle/Solution.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+        
+        for (int row = n - 2; row >= 0; row--) {
+            List<Integer> currentRow = triangle.get(row);
+            for (int col = 0; col < currentRow.size(); col++) {
+                dp[col] = currentRow.get(col) + Math.min(dp[col], dp[col + 1]);
+            }
+        }
+        
+        return dp[0];
+    }
+}
